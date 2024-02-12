1384d9f830bfed03e951976e8f1a64d24e396f32
Abylkhayir <abulhair097@gmail.com>
2024-02-12 17:47:58 +0600

    update

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,27 +3,27 @@ import { AuthService } from '../../authorization/authorization.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css',
-  standalone: true,
-})
-export class HeaderComponent {
-  toolBarOptionsBackground: string[] = [];
-  toolBarOptionsBorderBottom: string[] = [];
-  previousVal: string = 'ratio';
-  constructor(private AuthService: AuthService, private router: Router) {
-    this.toolBarOptionsBackground[this.previousVal] = '#333';
-    this.toolBarOptionsBorderBottom[this.previousVal] = '3px solid #CCCCCC';
-  }
-  onSelect(e) {
-    this.toolBarOptionsBackground[e] = '#333';
-    this.toolBarOptionsBorderBottom[e] = '3px solid #CCCCCC';
-    this.toolBarOptionsBackground[this.previousVal] = '#222222';
-    this.toolBarOptionsBorderBottom[this.previousVal] = 'none';
-    this.previousVal = e;
-  }
-  logout() {
-    this.AuthService.logout();
-  }
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrl: './header.component.scss',
+	standalone: true,
+	})
+	export class HeaderComponent {
+	toolBarOptionsBackground: string[] = [];
+	toolBarOptionsBorderBottom: string[] = [];
+	previousVal: string = '';
+	constructor(private AuthService: AuthService, private router: Router) {
+		this.toolBarOptionsBackground[this.previousVal] = '#333';
+		this.toolBarOptionsBorderBottom[this.previousVal] = '3px solid #CCCCCC';
+	}
+	onSelect(e) {
+		this.toolBarOptionsBackground[e] = '#333';
+		this.toolBarOptionsBorderBottom[e] = '3px solid #CCCCCC';
+		this.toolBarOptionsBackground[this.previousVal] = '#222222';
+		this.toolBarOptionsBorderBottom[this.previousVal] = 'none';
+		this.previousVal = e;
+	}
+	logout() {
+		this.AuthService.logout();
+	}
 }
