9a22ec52d2e8d473b7ae30a24a4e899d8fb8a119
Abylkhayir <abulhair097@gmail.com>
2024-02-12 10:58:55 +0600

    Add commit

--- src/app/authorization/authorization.service.ts
+++ src/app/authorization/authorization.service.ts
@@ -15,7 +15,8 @@ export class AuthService {
   authenticate(login: string, password: string): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}`, { login, password });
     //return of ({ token: 'adadadada' });
-  }
+	 // Если Api не работает то вторую строчку,если работает то первую  
+	}
 
   isAuthenticated(): boolean {
     const token = localStorage.getItem(TOKEN);
